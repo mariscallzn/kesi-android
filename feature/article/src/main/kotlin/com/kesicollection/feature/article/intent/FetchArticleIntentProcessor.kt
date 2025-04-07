44384117aceaf1a2e9e168b325d4587e4dac501c
Andres Mariscal <mariscal.lzn@gmail.com>
2025-04-07 07:34:38 -0500

    chore(article): Refactor `FetchArticleIntentProcessor` with detailed documentation
    - Added detailed documentation to `FetchArticleIntentProcessor` explaining its purpose, functionality, and error handling.
    - Improved clarity on how the processor fetches and handles article data using `GetArticleByIdUseCase`.
    - Clarified how the processor updates the `Reducer` based on the success or failure of fetching the article.

--- feature/article/src/main/kotlin/com/kesicollection/feature/article/intent/FetchArticleIntentProcessor.kt
+++ feature/article/src/main/kotlin/com/kesicollection/feature/article/intent/FetchArticleIntentProcessor.kt
@@ -11,6 +11,28 @@ import com.kesicollection.feature.article.components.Paragraph
 import com.kesicollection.feature.article.components.SubHeader
 import com.kesicollection.feature.article.uimodel.UiPodcast
 
+/**
+ * [IntentProcessor] responsible for fetching an article by its ID.
+ *
+ * This class uses the [GetArticleByIdUseCase] to retrieve the article data
+ * and updates the [Reducer] accordingly with the fetched content or any errors.
+ *
+ * The processing happens in the following steps:
+ *
+ * 1.  **Loading State:** It initially sets the `isLoading` flag in the [Reducer] to `true` and clears any previous errors.
+ * 2.  **Fetch Article:** It uses the [GetArticleByIdUseCase] to fetch the article data associated with the provided [articleId].
+ * 3.  **Successful Retrieval:** If the article is fetched successfully:
+ *     *   It updates the [Reducer] with the article's `title`, `imageUrl`, and `content`.
+ *     *   The content, which is a list of `ContentSection` models, is mapped to a list of UI components, like [BulletList], [Code], [Paragraph], and [SubHeader].
+ *     * The podcast information is mapped to a [UiPodcast].
+ *     *   It sets `isLoading` to `false` in the [Reducer].
+ * 4.  **Error Handling:** If an exception occurs during the fetching process:
+ *     *   It updates the [Reducer] with an [ErrorState], including the error type ([ArticleErrors.NetworkError]) and the error message.
+ *     *   It sets `isLoading` to `false` in the [Reducer].
+ *
+ * @property articleId The ID of the article to be fetched.
+ * @property getArticleByIdUseCase The use case for retrieving article data by ID.
+ */
 class FetchArticleIntentProcessor(
     private val articleId: String,
     private val getArticleByIdUseCase: GetArticleByIdUseCase,
